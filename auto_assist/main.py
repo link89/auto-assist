05960f0dbe27a691971fc9a230726e1a6b88aaa3
weihong.xu <xuweihong.cn@qq.com>
2023-09-20 15:54:06 +0800

    wip: explore author network

--- auto_assist/main.py
+++ auto_assist/main.py
@@ -5,10 +5,11 @@ from playwright.async_api import async_playwright
 import asyncio
 import os
 import fire
+from contextlib import asynccontextmanager
 
 from .browser import lauch_browser
 from .lib import USER_HOME
-
+from .tasks import explore_author_network
 
 
 class Entry:
@@ -19,26 +20,49 @@ class Entry:
         if os.path.exists(config_file):
             yaml = YAML(typ='safe')
             with open(config_file) as f:
-                self.config = yaml.load(f)
+                self._config = yaml.load(f)
         else:
-            self.config = {}
+            self._config = {}
 
     def browser(self):
-        return BrowserCmd(self.config.get('browsers', {}))
+        return BrowserCmd(self)
+
+    def task(self):
+        return TaskCmd(self)
+
+    def _get_browser_config(self, name: str):
+        return self._config.get('browsers', {}).get(name, {})
 
 
 class BrowserCmd:
 
-    def __init__(self, config: dict) -> None:
-        self.config = config
+    def __init__(self, entry: Entry) -> None:
+        self._entry = entry
 
     def launch(self, name: str = 'default'):
-        config = self.config.get(name, {})
-        async def _launch():
-            async with async_playwright() as pw:
-                await lauch_browser(name, config, USER_HOME)(pw)
+        async def run():
+            async for _ in self._launch_async(name):
                 input('Press any key to exit ...')
-        asyncio.run(_launch())
+        asyncio.run(run())
+
+    @asynccontextmanager
+    async def _launch_async(self, name: str):
+        config = self._entry._get_browser_config(name)
+        async with async_playwright() as pw:
+            yield await lauch_browser(name, config, USER_HOME)(pw)
+
+
+class TaskCmd:
+
+    def __init__(self, entry: Entry) -> None:
+        self._entry = entry
+
+    def explore_author_network(self, browser='default'):
+        async def run():
+            async with self._entry.browser()._launch_async(browser) as _browser:
+                await explore_author_network.run(_browser)
+                input('Press any key to exit ...')
+        asyncio.run(run())
 
 
 if __name__ == '__main__':
