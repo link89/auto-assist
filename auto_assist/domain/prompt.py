48a633124564ad5e21a8d34cfadcfeafcda96967
weihong.xu <xuweihong.cn@qq.com>
2025-02-13 16:33:52 +0800

    optimize prompt

--- auto_assist/domain/prompt.py
+++ auto_assist/domain/prompt.py
@@ -216,4 +216,5 @@ Notes:
 * The markdown file may contain information of multiple students, in this case you ignore all other students and only focus on the mentioned student at the beginning of the input.
 * Note that if the page is Google Scholar page, you can just return name, email, and publications of the student, and ignore other information, because Google Scholar page usually does not contain detailed information of the student.
 * The markdwon file may not be the profile of the mentioned person. In this case, you should judge whether the markdown file is the profile of the mentioned at the beginning of the input, if the name and insitute is not matched or have nothing to do with chemistry, you should explain why return an empty json block.
+* There is the chance that the markdown file is a members page which contains multiple students, in this case, you should return the information of the mentioned student and ignore all other students.
 """.strip()).substitute(CHEMISTRY_RESEARCH_AREA=CHEMISTRY_RESEARCH_AREA)
